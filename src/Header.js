094227fa745c569f4f53321606b0b1369fc96be5
Kenny Zhao <53667317+mzha999@users.noreply.github.com>
2021-09-21 17:07:14 +1200

    deploy

---
+++ src/Header.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateContext";
+import {auth} from "./firebase"
+
+const Header = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
+  console.log(user)
+  const handleAuthentication = ()=> {
+    if(user){
+      auth.signOut();
+    }
+  }
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="logo"
+          className="header_logo"
+        />
+      </Link>
+
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon"></SearchIcon>
+      </div>
+      <div className="header_nav">
+        <Link to={!user && '/login'}>
+          <div className="header_option" onClick={handleAuthentication}>
+            <span className="header_optionLineOne">{user? `Hello ${user.displayname}`: 'Hello guest'}</span>
+            <span className="header_optionLineTwo">{user?"Sign Out":"Sign In"} </span>
+          </div>
+        </Link>
+
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header_basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
