5ab6a6841f1be9f1c3c1fe198997e43339ab7c40
Kenny Zhao <53667317+mzha999@users.noreply.github.com>
2021-09-21 19:44:20 +1200

    deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   const [{}, dispatch] = useStateValue()
   useEffect(()=>{
     auth.onAuthStateChanged(authUser => {
-      console.log(authUser)
       if(authUser) {
         dispatch({
           type: "SET_USER",
